fcb4ee62d5635faf3de88780d4c722cd3eab267f
Ole Asheim Askeland <oleaask@stud.ntnu.no>
2022-02-08 15:29:57 +0100

    Update FriendtripApplication.java

--- backend/src/main/java/Backend/Friendtrip/FriendtripApplication.java
+++ backend/src/main/java/Backend/Friendtrip/FriendtripApplication.java
@@ -1,12 +1,8 @@
 package Backend.Friendtrip;
 
 //Databaserelaterte imports
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 //
 import org.springframework.boot.SpringApplication;
