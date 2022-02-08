8de8f87977c73b1320d8756bb125e916d6f69627
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-02-08 15:41:03 +0100

    Fix database communication in test

--- backend/src/test/java/Backend/Friendtrip/FriendtripApplicationTests.java
+++ backend/src/test/java/Backend/Friendtrip/FriendtripApplicationTests.java
@@ -1,14 +1,6 @@
 package Backend.Friendtrip;
 
 
-//Databaserelaterte imports
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,30 +13,7 @@ class FriendtripApplicationTests {
 	}
 
 }
-//Snakke med database via JDBC template
-@SpringBootApplication
-class SQLCON implements CommandLineRunner {
 
-	@Autowired
-    private JdbcTemplate jdbcTemplate;
-     
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
- 
-    @Override
-    public void run(String... args) throws Exception {
-        String sql = "INSERT INTO Users (Name) VALUES (?)"; //Gitt at du har laget en tabell allerede
-         
-        int result = jdbcTemplate.update(sql, "Ole");
-         
-        if (result > 0) {
-            System.out.println("A new row has been inserted.");
-        }
-         
-    }
-
-}
 	
 	
 
