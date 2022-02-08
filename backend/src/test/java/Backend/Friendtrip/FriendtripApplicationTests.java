ffd3b52d66dd20c50929064d164e6080815ecb2e
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-02-08 14:39:43 +0100

    #9 Possible setup implementation

---
+++ backend/src/test/java/Backend/Friendtrip/FriendtripApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Friendtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
