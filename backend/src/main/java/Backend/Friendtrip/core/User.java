80db25cff80940e015aadd8c9b923aaf622ef257
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-02-10 19:17:21 +0100

    #4 Create user class and set up test class
    Co-authored-by: Marcus

--- backend/src/main/java/Backend/Friendtrip/core/User.java
+++ backend/src/main/java/Backend/Friendtrip/core/User.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import java.util.regex.*;
+
 @Entity
 public class User {
     
@@ -17,5 +19,38 @@ public class User {
     public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
+    }
+    
+    public void setUserName(String userName) throws IllegalArgumentException {
+        
+        //Add functionality for distinct users
+        if (userName.matches("^[a-zA-Z]*$") && (userName != null) 
+        && (!userName.equals("")) && (userName.length() < 30)) {
+
+            this.userName = userName;
+        }
+        else {
+            throw new IllegalArgumentException("User name  is not valid");
+        }
+       
+        
+    }
+    
+    public void setPassword(String password) {
+
+        if (password.length() > 7) {
+            int count = 0;
+            for (int i = 0; i < password.length(); i++) {
+                if (!Character.isLetter(password.charAt(i)) && !Character.isDigit(password.charAt(i))) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                throw new IllegalArgumentException(
+                    "The password does not contain a special character or is under 8 letters");
+            }
+            this.password = password;
+        }
+
     }
 }
---
+++ backend/src/test/java/Backend/Friendtrip/core/UserTest.java
@@ -0,0 +1,9 @@
+package backend.friendtrip.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class UserTest {
+
+}
