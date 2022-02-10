49b769bb98fe2cdfa02df07eb1a4dca33f686c1d
ole hermann bakka <olehbak@stud.ntnu.no>
2022-02-11 00:40:32 +0100

    vidre utvikling av GUI med storybook react

--- my-app/taskbox/src/App.js
+++ my-app/taskbox/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -21,5 +22,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
