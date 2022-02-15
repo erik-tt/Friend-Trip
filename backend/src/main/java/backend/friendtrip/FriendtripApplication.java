ad446569cba6607d8c63bf161b4696eea6916743
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-02-15 15:07:35 +0100

    #3 API server implementation working test

--- backend/src/main/java/backend/friendtrip/FriendtripApplication.java
+++ backend/src/main/java/backend/friendtrip/FriendtripApplication.java
@@ -20,16 +20,19 @@ public class FriendtripApplication implements CommandLineRunner {
     //It finds two main classes and wont compile
    public static void main(String[] args) {
         SpringApplication.run(FriendtripApplication.class, args);
+        
     }
  
     @Override
     public void run(String... args) throws Exception {
         UserRepo.save( new User("askeland", "123456789!"));
+        UserRepo.save( new User("Erik", "ErikTT12121!"));
 
     
         
         
         System.out.println(UserRepo.existsByuserName("askelandsdsda"));
+        System.out.println(UserRepo.existsByuserName("askeland"));
         System.out.println(UserRepo.findByuserName("askeland").getUserName());
         
         
