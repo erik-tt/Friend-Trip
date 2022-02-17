46f4920aa625d3742f1d15b8c8636a4e1a5c4002
philiptinius <philiptinius@gmail.com>
2022-02-17 13:49:43 +0100

    Set up react working with API

--- backend/src/main/java/backend/friendtrip/FriendtripController.java
+++ backend/src/main/java/backend/friendtrip/FriendtripController.java
@@ -1,20 +1,14 @@
 package backend.friendtrip;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
-@RequestMapping("/api/v2")
+
+@Controller
 public class FriendtripController {
 
-    @GetMapping(value = "/test", produces = "application/json")
-    public String addTest() {
-        return "Dette er en test";
-    }
-
+    @RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
 }
