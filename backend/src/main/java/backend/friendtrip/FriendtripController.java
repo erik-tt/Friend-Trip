6efd5f1ae09afa848ac1d7a8c95e75e33d8c9c79
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-02-25 09:52:01 +0100

    #24 Effort to add cross origin support

--- backend/src/main/java/backend/friendtrip/FriendtripController.java
+++ backend/src/main/java/backend/friendtrip/FriendtripController.java
@@ -1,10 +1,12 @@
 package backend.friendtrip;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@CrossOrigin(origins="http://localhost:3000")
 public class FriendtripController {
 
     @RequestMapping(value = "/")
