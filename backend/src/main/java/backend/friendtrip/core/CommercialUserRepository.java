21ec248d08d371eb8d280ba6864603aa05974d91
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-03-22 14:41:20 +0100

    #35 Delete commercial user and add attributes user
    Co-authored-by: Eiril Solveig Ugulen <eirilsu@ntnu.no>
    Co-authored-by: Ole Asheim Askeland <oleaask@ntnu.no>

--- backend/src/main/java/backend/friendtrip/core/CommercialUserRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.friendtrip.core;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommercialUserRepository extends CrudRepository<CommercialUser, Long>  {
-
-  boolean existsByUsername(String username);
-  CommercialUser findByUsername(String username);
-}
